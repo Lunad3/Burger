410862319c980c2a0d2ed82718f3d4b7b081d201
LunaPC <lunad3@uci.edu>
2018-06-20 20:09:35 -0700

    router in burgers_controller.js created

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,2 +1,31 @@
 var express = require("express");
 var router = express.Router();
+var Burgers = require("../models/burger.js");
+
+router.get("/",function(req,res){
+    Burgers.selectAll(function(data){
+        var burgersObj = {
+            burgers: data
+        };
+        console.log(allBurgers);
+        res.render("index",burgersObj);
+    });
+});
+
+router.post("/api/burgers",function(req,res){
+    Burgers.insertOne(req.body.burger_name,function(result){
+        res.json({id: result.id});
+    });
+});
+
+router.put("/api/burgers/:id",function(req,res){
+    console.log("condition: id=" + req.params.id);
+    Burgers.updateOne(req.params.id,req.body.burger_name,function(result){
+        if(result.changedRows === 0){
+            return res.status(404).end();
+        }
+        res.status(200).end();
+    });
+});
+
+module.exports = router;
