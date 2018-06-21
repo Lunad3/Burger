6986e3a1ec9ef9aa59e6062b9aeb176ec1c4fb74
LunaPC <lunad3@uci.edu>
2018-06-21 01:41:22 -0700

    edited connection.js so allow JAWSDB connection

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,16 @@
 var mysql = require("mysql");
-
-var db = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "Luna",
-    password: "Papaya321",
-    database:"burgers_db"
-});
+var db;
+if (process.env.JAWSDB_URL){
+    db = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+    db = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "Luna",
+        password: "Papaya321",
+        database:"burgers_db"
+    });
+};
 
 db.connect(function(err){
     if (err){
